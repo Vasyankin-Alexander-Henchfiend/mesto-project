c8a4106ccac2084da41415841a2f50f5ed8c5bc7
Александр Васянькин <henchfiend@yandex.ru>
2022-10-29 22:45:35 +0300

    js step 3

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
+//подключаем функцию открытия/закрытия окна редактирования профиля
 let editButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let popupCloseButton = document.querySelector('.popup__close-icon');
 
-let profileForm = document.querySelector('.profile__info');
-let profileName = profileForm.querySelector('.profile__title');
-let profileStatus = profileForm.querySelector('.profile__subtitle');
+
 
 function popupOpened () {
     popup.classList.add('popup_opened');
@@ -18,6 +17,10 @@ function popupClosed () {
 
 popupCloseButton.addEventListener('click', popupClosed);
 
+//подключаем функцию редактирования информации профиля
+let profileForm = document.querySelector('.profile__info');
+let profileName = profileForm.querySelector('.profile__title');
+let profileStatus = profileForm.querySelector('.profile__subtitle');
 
 const formElement = document.querySelector('.popup__form');
 const nameInput = formElement.querySelector('#name');
@@ -29,11 +32,43 @@ function formSubmitHandler (evt) {
   profileName.textContent = nameInput.value;
   profileStatus.textContent = jobInput.value;
   popup.classList.remove('popup_opened');
-  
-// Получите значение полей jobInput и nameInput из свойства value
-// Выберите элементы, куда должны быть вставлены значения полей 
-// Вставьте новые значения с помощью textContent
-
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
+
+//подключаем подгрузку картинок
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+    ];
+
+    //подключаем лайки
+    let cardLikeButtons = document.querySelectorAll('.element__like-button');
+
+    function cardLikeButtonActive () {
+        target.cardLikeButtons.classList.add('element__like-button_active');
+    }
+    
+    cardLikeButtons.addEventListener('click', cardLikeButtonActive);
