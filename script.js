b75fa67a7e1ae9b2663ef307037a4d43182375d4
Александр Васянькин <henchfiend@yandex.ru>
2022-10-31 15:49:46 +0300

    js step 5

--- script.js
+++ script.js
@@ -3,32 +3,13 @@ const editButton = document.querySelector('.profile__edit-button');
 const popupEditProfile = document.querySelector('.popup_type_edit-profile');
 const popupEditProfileCloseButton = document.querySelector('#close-edit-form');
 
-
-function popupEditProfileOpened () {
+editButton.addEventListener('click', function() {
   popupEditProfile.classList.add('popup_opened');
-}
-editButton.addEventListener('click', popupEditProfileOpened);
+});
 
-function popupEditProfileClosed () {
+popupEditProfileCloseButton.addEventListener('click', function() {
   popupEditProfile.classList.remove('popup_opened');
-}
-popupEditProfileCloseButton.addEventListener('click', popupEditProfileClosed);
-
-
-//подключаем функцию открытия/закрытия окна добавления картинок
-const addButton = document.querySelector('.profile__add-button');
-const popupAddElement = document.querySelector('.popup_type_add-element');
-const popupAddElementCloseButton = document.querySelector('#close-add-form');
-
-function popupAddElementOpened () {
-  popupAddElement.classList.add('popup_opened');
-}
-addButton.addEventListener('click', popupAddElementOpened);
-
-function popupAddElementClosed () {
-  popupAddElement.classList.remove('popup_opened');
-}
-popupAddElementCloseButton.addEventListener('click', popupAddElementClosed);
+});
 
 
 //подключаем функцию редактирования информации профиля
@@ -36,7 +17,7 @@ const profileForm = document.querySelector('.profile__info');
 const profileName = profileForm.querySelector('.profile__title');
 const profileStatus = profileForm.querySelector('.profile__subtitle');
 
-const formElement = document.querySelector('.popup__form');
+const formElement = document.querySelector('.popup__form_type_edit-form');
 const nameInput = formElement.querySelector('.popup__form-input_type_edit-name');
 const jobInput = formElement.querySelector('.popup__form-input_type_edit-status');
 
@@ -87,4 +68,53 @@ const initialCards = [
     
     // cardLikeButtons.addEventListener('click', cardLikeButtonActive);
 
-    
+//подключаем функцию открытия/закрытия окна добавления картинок
+const addButton = document.querySelector('.profile__add-button');
+const popupAddElement = document.querySelector('.popup_type_add-element');
+const popupAddElementCloseButton = document.querySelector('#close-add-form');
+
+addButton.addEventListener('click', function () {
+  popupAddElement.classList.add('popup_opened');
+});
+
+popupAddElementCloseButton.addEventListener('click', function () {
+  popupAddElement.classList.remove('popup_opened');
+});
+
+
+
+//подключаем функцию добавления картинок
+const elements = document.querySelector('.elements');
+const elementAddForm = document.querySelector('.popup__form_type_add-form');
+// const elementAddSaveButton = elementAddForm.querySelector('.popup__save-button');
+
+function addElement(nameValue, sourceValue) {
+  const elementTemplate = document.querySelector('#element-template').content;
+  const element = elementTemplate.querySelector('.element').cloneNode(true);
+
+  element.querySelector('.element__title').textContent = nameValue;
+  element.querySelector('.element__image').src = sourceValue;
+  
+  element.querySelector('.element__like-button').addEventListener('click', function (evt) {
+     evt.target.classList.toggle('element__like-button_active');
+  });
+  
+elements.prepend(element); 
+};
+
+
+
+
+function save(evt) {
+  evt.preventDefault(); 
+  const name = document.querySelector('.popup__form-input_type_add-name');
+  const source = document.querySelector('.popup__form-input_type_add-source');
+
+  addElement(name.value, source.value);
+  // renderHasSongs();
+
+  name.value = '';
+  source.value = '';
+};
+
+elementAddForm.addEventListener('submit', save);
