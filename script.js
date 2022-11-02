166badcf29a2fbdf0aa6be291052aed318b1d3e2
Александр Васянькин <henchfiend@yandex.ru>
2022-11-02 14:44:42 +0300

    js step 9
    - все функции добавлены и протестированы

--- script.js
+++ script.js
@@ -28,35 +28,57 @@ const initialCards = [
 
   const elements = document.querySelector('.elements');
 
-  initialCards.forEach((Card) => { 
-    const elementTemplate = document.querySelector('#element-template').content;
-    const element = elementTemplate.querySelector('.element').cloneNode(true);
+initialCards.forEach((Card) => { 
+  const elementTemplate = document.querySelector('#element-template').content;
+  const element = elementTemplate.querySelector('.element').cloneNode(true);
   
-    element.querySelector('.element__title').textContent = Card.name;
-    element.querySelector('.element__image').src = Card.link;
+  element.querySelector('.element__title').textContent = Card.name;
+  element.querySelector('.element__image').src = Card.link;
+  element.querySelector('.element__image').alt = Card.name;
     
-    element.querySelector('.element__delete').addEventListener('click', function(evt) { element.remove() });
+  element.querySelector('.element__delete').addEventListener('click', function(evt) { element.remove() });
   
-    element.querySelector('.element__like-button').addEventListener('click', function (evt) {
-       evt.target.classList.toggle('element__like-button_active');
-    });
-   
-    elements.prepend(element); 
-
+  element.querySelector('.element__like-button').addEventListener('click', function (evt) {
+    evt.target.classList.toggle('element__like-button_active');
   });
+   
+  elements.prepend(element); 
+
+  //Функция просмотра картинки во всплывающем окне
+  const images = document.querySelectorAll('.element__image');
+  const popupImage = document.querySelector('.popup_type_image');
 
 
-//подключаем функцию открытия/закрытия окна редактирования профиля
+  images.forEach(image => {
+    image.addEventListener('click', () => {
+      popupImage.querySelector('.popup__image').src = image.src
+      popupImage.querySelector('.popup__caption').textContent = image.alt
+  
+      popupImage.classList.add('popup_opened');
+    });
+  });
+});
+
+//подключаем функцию закрытия любого окна popup
+const popupCloseButtons = document.querySelectorAll('.popup__close-icon');
+const popups = document.querySelectorAll('.popup');
+
+popupCloseButtons.forEach(popupCloseButton => {
+  popupCloseButton.addEventListener('click', () => {
+    popups.forEach(popup => {
+    popup.classList.remove('popup_opened');
+  });
+  });
+});
+
+
+//подключаем функцию открытия окна редактирования профиля
 const editButton = document.querySelector('.profile__edit-button');
 const popupEditProfile = document.querySelector('.popup_type_edit-profile');
 const popupEditProfileCloseButton = document.querySelector('#close-edit-form');
 
 editButton.addEventListener('click', function() {
   popupEditProfile.classList.add('popup_opened');
-});
-
-popupEditProfileCloseButton.addEventListener('click', function() {
-  popupEditProfile.classList.remove('popup_opened');
 });
 
 
@@ -75,11 +97,11 @@ function formSubmitHandler (evt) {
   profileName.textContent = nameInput.value;
   profileStatus.textContent = jobInput.value;
   popupEditProfile.classList.remove('popup_opened');
-}
+};
 
 formElement.addEventListener('submit', formSubmitHandler);
 
-//подключаем функцию открытия/закрытия окна добавления картинок
+//подключаем функцию открытия окна добавления картинок
 const addButton = document.querySelector('.profile__add-button');
 const popupAddElement = document.querySelector('.popup_type_add-element');
 const popupAddElementCloseButton = document.querySelector('#close-add-form');
@@ -87,11 +109,6 @@ const popupAddElementCloseButton = document.querySelector('#close-add-form');
 addButton.addEventListener('click', function () {
   popupAddElement.classList.add('popup_opened');
 });
-
-popupAddElementCloseButton.addEventListener('click', function () {
-  popupAddElement.classList.remove('popup_opened');
-});
-
 
 
 //подключаем функцию добавления картинок
@@ -113,6 +130,19 @@ function addElement(nameValue, sourceValue) {
  
   elements.prepend(element); 
   popupAddElement.classList.remove('popup_opened');
+
+  //Функция просмотра картинки во всплывающем окне
+  const images = document.querySelectorAll('.element__image');
+  const popupImage = document.querySelector('.popup_type_image');
+
+  images.forEach(image => {
+    image.addEventListener('click', () => {
+      popupImage.querySelector('.popup__image').src = image.src
+      popupImage.querySelector('.popup__caption').textContent = image.alt
+  
+      popupImage.classList.add('popup_opened');
+    });
+  });
 };
 
 elementAddForm.addEventListener('submit', function(evt) {
