e40b00f0aa11fdfc311c30029510c503d01becb1
Александр Васянькин <henchfiend@yandex.ru>
2022-11-02 08:49:02 +0300

    js step 8

--- script.js
+++ script.js
@@ -1,3 +1,51 @@
+//подключаем подгрузку картинок
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+  ];
+
+  const elements = document.querySelector('.elements');
+
+  initialCards.forEach((Card) => { 
+    const elementTemplate = document.querySelector('#element-template').content;
+    const element = elementTemplate.querySelector('.element').cloneNode(true);
+  
+    element.querySelector('.element__title').textContent = Card.name;
+    element.querySelector('.element__image').src = Card.link;
+    
+    element.querySelector('.element__delete').addEventListener('click', function(evt) { element.remove() });
+  
+    element.querySelector('.element__like-button').addEventListener('click', function (evt) {
+       evt.target.classList.toggle('element__like-button_active');
+    });
+   
+    elements.prepend(element); 
+
+  });
+
+
 //подключаем функцию открытия/закрытия окна редактирования профиля
 const editButton = document.querySelector('.profile__edit-button');
 const popupEditProfile = document.querySelector('.popup_type_edit-profile');
@@ -31,34 +79,6 @@ function formSubmitHandler (evt) {
 
 formElement.addEventListener('submit', formSubmitHandler);
 
-//подключаем подгрузку картинок
-const initialCards = [
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-    ];
-
 //подключаем функцию открытия/закрытия окна добавления картинок
 const addButton = document.querySelector('.profile__add-button');
 const popupAddElement = document.querySelector('.popup_type_add-element');
@@ -75,7 +95,7 @@ popupAddElementCloseButton.addEventListener('click', function () {
 
 
 //подключаем функцию добавления картинок
-const elements = document.querySelector('.elements');
+
 const elementAddForm = document.querySelector('.popup__form_type_add-form');
 
 function addElement(nameValue, sourceValue) {
