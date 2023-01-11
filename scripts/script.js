859038ebbfa0bcacea28ab520465b86c7afdedb3
Александр Васянькин <henchfiend@yandex.ru>
2023-01-11 15:26:57 +0300

    подключение валидации форм ч.1
    - портирован код валидации
    - грубо подключен показ текстовых подсказок на форме добавления картинки
    - стили подсказок скопированы с примера и требуют приведения к макету

--- scripts/script.js
+++ scripts/script.js
@@ -55,23 +55,26 @@ function openPopup(popup) {
   popup.classList.add('popup_opened');
 }
 
+/////////////////////////////////////////////////////////////////////////
+/*Функции закрытия окон*/
 function closePopup(popup) {
   popup.classList.remove('popup_opened');
 }
 
-function closePopupWhithEscape(evt) {  
+popupCloseButtons.forEach((button) => {
+  const popup = button.closest('.popup');
+  button.addEventListener('click', () => closePopup(popup));
+});
+
+function closeByEscape(evt) {  
   const popupOpen = document.querySelector('.popup_opened');
   if (evt.key === "Escape") {
     closePopup(popupOpen);
   }
 }
 
-document.addEventListener('keydown', closePopupWhithEscape);
-
-popupCloseButtons.forEach((button) => {
-  const popup = button.closest('.popup');
-  button.addEventListener('click', () => closePopup(popup));
-});
+document.addEventListener('keydown', closeByEscape);
+//////////////////////////////////////////////////////////////////////////
 
 editButton.addEventListener("click", function () {
   nameInput.value = profileName.textContent.trim();
@@ -134,3 +137,69 @@ function handleProfileFormSubmit (evt) {
 };
 
 profileForm.addEventListener('submit', handleProfileFormSubmit);
+
+
+//////////////////////////////////////////////
+/*Тупо валидация */
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add('form__input_type_error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('popup__form-input-error_active');
+};
+
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove('form__input_type_error');
+  errorElement.classList.remove('popup__form-input-error_active');
+  errorElement.textContent = '';
+};
+
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll('.popup__form-input'));
+  const buttonElement = formElement.querySelector('.popup__save-button');
+  toggleButtonState(inputList, buttonElement);
+  
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', function () {
+      checkInputValidity(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  }); 
+}
+
+const toggleButtonState = (inputList, buttonElement) => {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add('popup__save-button_inactive');
+  } else {
+    buttonElement.classList.remove('popup__save-button_inactive'); /*этот класс надо создать */
+  }
+}
+
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll('.popup__form'));
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', function (evt) {
+      evt.preventDefault();
+    });
+    
+    setEventListeners(formElement);
+  });
+};
+
+enableValidation();
+/////////////////////////////////////////////
